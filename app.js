409e01ad5af35e43253abe1c918f8ba4a4851a49
Jim Robinson <jscrobinson@gmail.com>
2018-02-27 22:17:37 +0000

    Test build pipeline

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send({
-    "Output": "Hello World!"
-  });
+    res.send({
+        "Output": "Hello World!"
+    });
 });
 
 app.post('/', function(req, res) {
-  res.send({
-    "Output": "Hello World!"
-  });
+    res.send({
+        "Output": "Hello World! Test"
+    });
 });
 
 
